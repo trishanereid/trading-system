7e05ab249b44fbab3dbbe5bbeadd59e7b656e1b8
Trishane Reid <trishanereid17@gmail.com>
2024-12-30 00:49:50 +0530

    initialized project files

---
+++ services/market-data-service/src/test/java/edu/neuralfintech/market_data_service/MarketDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neuralfintech.market_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
